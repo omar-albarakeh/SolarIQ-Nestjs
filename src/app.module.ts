09557a12f48479de9222a90bcbbd84eb4f98ecbb
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 00:06:55 +0200

    Nestjs <> main updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './User/user.module';
+import { envValidationSchema } from './JWT-Strategy/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath: '.env', 
+      validationSchema: envValidationSchema, 
+      validationOptions: {
+        allowUnknown: true, 
+        abortEarly: false, 
+      },
+    }),
 
+    MongooseModule.forRoot(process.env.DB_URI || '', {
+      retryAttempts: 5,
+      retryDelay: 1000, 
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
-bootstrap();
