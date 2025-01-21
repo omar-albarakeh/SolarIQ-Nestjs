ffb2f734f6d3fbb005b5fe7b08530dc9441433d6
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 00:52:12 +0200

    Nestjs <> update user dto updated

--- src/User/dto/updateuser.dto.ts
+++ src/User/dto/updateuser.dto.ts
@@ -4,11 +4,11 @@ import { UserType } from '../user_type.enum';
 export class SignUpDto {
   @IsNotEmpty({ message: 'Name is required' })
   @IsString({ message: 'Name must be a string' })
-  readonly name: string;
+  readonly name?: string;
 
   @IsNotEmpty({ message: 'Email is required' })
   @IsEmail({}, { message: 'Invalid email address' })
-  readonly email: string;
+  readonly email?: string;
 
   @IsNotEmpty({ message: 'Password is required' })
   @IsString({ message: 'Password must be a string' })
@@ -17,17 +17,17 @@ export class SignUpDto {
     message:
       'Password must contain at least one uppercase letter, one number, and one special character',
   })
-  readonly password: string;
+  readonly password?: string;
 
   @IsNotEmpty({ message: 'User type is required' })
   @IsEnum(UserType, { message: 'Invalid user type. Valid options are: Admin, Engineer, User' })
-  readonly type: UserType;
+  readonly type?: UserType;
 
   @IsNotEmpty({ message: 'Phone is required' })
   @Matches(/^\d{8}$/, { message: 'Phone must be a valid 8-digit number' })
-  readonly phone: string;
+  readonly phone?: string;
 
   @IsNotEmpty({ message: 'Address is required' })
   @IsString({ message: 'Address must be a string' })
-  readonly address: string;
+  readonly address?: string;
 }
