eaacf702d5683a9d96641b2e6ecc11c4f5bc2c56
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 00:13:36 +0200

    Nestjs <> repository updated

--- src/User/user.repositories.ts
+++ src/User/user.repositories.ts
@@ -7,12 +7,15 @@ import { User } from './user.schema';
 export class UserRepository {
   constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
-  async createUser(userData: Partial<User>): Promise<User> {
+    async createUser(userData: Partial<User>): Promise<User> {
     const user = new this.userModel(userData);
     return await user.save();
   }
 
-  
+  async findUserByEmail(email: string): Promise<User | null> {
+    return await this.userModel.findOne({ email }).exec();
+  }
+
   async isEmailTaken(email: string): Promise<boolean> {
     const user = await this.userModel.findOne({ email }).exec();
     return !!user;
