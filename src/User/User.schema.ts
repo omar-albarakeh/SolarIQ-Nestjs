c0d46fce81e835cf5b84ed6fe0d60ea7105acf5b
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-21 23:31:27 +0200

    Nestjs <> user schema added

---
+++ src/User/User.schema.ts
@@ -0,0 +1,31 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  type: string;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop({ default: false })
+  isSolarInfoComplete: boolean;
+
+  @Prop({ default: false })
+  blocked: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
