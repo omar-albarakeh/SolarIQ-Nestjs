db3b31411cf2bcd1d5d0d36156fe45cad7aa7f94
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 00:08:55 +0200

    Nestjs <> repository added

---
+++ src/User/user.repositories.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.schema';
+
+@Injectable()
+export class UserRepository {
+  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
+
+  async createUser(userData: Partial<User>): Promise<User> {
+    const user = new this.userModel(userData);
+    return await user.save();
+  }
+
+}
