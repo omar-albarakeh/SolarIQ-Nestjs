40ca03fe80285b3ba5dfa6eb0d2e60714a692ede
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 13:18:07 +0200

    Nestjs <> message schema added

---
+++ src/chatApp/schema/message.schema.ts
@@ -0,0 +1,24 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { User } from '../../User/Auth/user.schema';
+import { ChatRoom } from './chatroom.schema';
+
+@Schema()
+export class Message extends Document {
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  sender: User;
+
+  @Prop({ type: Types.ObjectId, ref: 'ChatRoom', required: true })
+  chatRoom: ChatRoom;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ default: Date.now })
+  timestamp: Date;
+
+  @Prop({ default: false })
+  read: boolean;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
