3754f851c0f843e5a880fe1b3364c8c353c2489d
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 03:07:47 +0200

    Nestjs <> community posts schema added

---
+++ src/Community/Schema/CommunityPost.schema.ts
@@ -0,0 +1,23 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { User } from '../../User/Auth/user.schema';
+
+@Schema()
+export class CommunityPost extends Document {
+  @Prop({ required: true, type: Types.ObjectId, ref: 'User' })
+  author: User;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
+  likes: User[];
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Comment' }], default: [] })
+  comments: Comment[];
+}
+
+export const CommunityPostSchema = SchemaFactory.createForClass(CommunityPost);
