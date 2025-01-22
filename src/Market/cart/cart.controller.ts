f9f09320dfc79d260e273f95e52440494a25711e
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 12:15:26 +0200

    Nestjs <> cart controller added

--- src/Market/cart/cart.controller.ts
+++ src/Market/cart/cart.controller.ts
@@ -11,10 +11,15 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { CartService } from './cart.service';
-
+import { CartDocument } from './Cart.Schema';
 
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Post('/:userId')
+  async createCart(@Param('userId', ParseUUIDPipe) userId: string): Promise<CartDocument> {
+    return this.cartService.createCart(userId);
+  }
 
 }
