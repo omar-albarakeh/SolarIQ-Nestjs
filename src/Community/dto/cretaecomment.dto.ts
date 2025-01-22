f255f3d69912aa0633389dde04a41158885ea8fb
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 08:04:03 +0200

    updated

--- src/Community/dto/cretaecomment.dto.ts
+++ src/Community/dto/cretaecomment.dto.ts
@@ -1,10 +1,11 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsMongoId } from 'class-validator';
 
 export class CreateCommentDto {
   @IsString()
   @IsNotEmpty()
   text: string;
 
+  @IsMongoId()
   @IsNotEmpty()
   postId: string;
 }
