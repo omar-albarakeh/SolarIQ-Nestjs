c0494935029e4d0993c58129f7237cbd64884d40
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 08:04:28 +0200

    Nestjs <> delete post dto added

--- src/Community/dto/delete.post.dto.ts
+++ src/Community/dto/delete.post.dto.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsMongoId, IsNotEmpty } from 'class-validator';
 
 export class DeletePostDto {
+  @IsMongoId()
   @IsNotEmpty()
   postId: string;
 }
