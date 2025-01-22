6ea4f65743ecbaece56cb7d2a5089d85ba68b302
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 12:56:17 +0200

    Nestjs <> solarnews controller added

---
+++ src/SolarNews/solarnews.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { SolarNewsService } from './solarnews.service';
+import { CreateSolarNewsDto } from './dto/addsolarnews';
+import { UpdateSolarNewsDto } from './dto/UpdateSolarNewsDto';
+import { isValidObjectId } from 'mongoose';
+
+@Controller('solar-news')
+export class SolarNewsController {
+  constructor(private readonly solarNewsService: SolarNewsService) {}
+
+
+}
