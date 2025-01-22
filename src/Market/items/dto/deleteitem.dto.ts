9236daa8d42a256d5ad1051bb9fdb4bc1529cc32
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 09:24:51 +0200

    Nestjs <> updated

--- src/Market/items/dto/deleteitem.dto.ts
+++ src/Market/items/dto/deleteitem.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class DeleteItemDto {
-  @IsString()
+  @IsString({ message: 'ID must be a string' })
+  @IsNotEmpty({ message: 'ID is required' })
   id: string;
 }
