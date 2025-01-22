8b198b453f7d3d2eef88797f09f03f81d7bf5e92
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 13:30:06 +0200

    Nestjs <> chat controller added

---
+++ src/chatApp/chat.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ChatService } from './chat.service';
+
+@Controller('chat')
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+
+}
