a7bfc5045d22cc046b345b5d15eb6ed23cd5ee33
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 09:40:01 +0200

    Nestjs <> market Controller added

---
+++ src/Market/items/itemController.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+  Query,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { ItemService } from './item.service';
+import { CreateItemDto } from './dto/createitem.dto';
+import { UpdateItemDto } from './dto/updateitem.dto';
+import { Item } from '../items/item.schema';
+
+@Controller('items')
+export class ItemController {
+  constructor(private readonly itemService: ItemService) {}
+
+}
