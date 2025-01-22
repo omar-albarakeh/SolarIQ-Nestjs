9579607f1e13ebcab0525a732b84f1767d4ff1b2
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 03:11:06 +0200

    Nestjs <> dto updated

---
+++ src/Community/dto/cretaecomment.dto.ts
@@ -0,0 +1,10 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsNotEmpty()
+  postId: string;
+}
