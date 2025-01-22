bc80d6209c1add23d91593b0dc7898db70a07710
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-22 13:24:18 +0200

    Nestjs <> chatroom repository added

--- src/chatApp/websocket.gatway.ts/WebSocketGateway.ts
+++ src/chatApp/websocket.gatway.ts/WebSocketGateway.ts
@@ -2,9 +2,9 @@ import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody } from
 import { Server } from 'socket.io';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Message } from '../message.schema';
-import { ChatRoom } from '../chatroom.schema';
-import { UserConnection } from '../userconnection.schema';
+import { Message } from '../schema/message.schema';
+import { ChatRoom } from '../schema/chatroom.schema';
+import { UserConnection } from '../schema/userconnection.schema';
 
 @WebSocketGateway()
 export class ChatGateway {
